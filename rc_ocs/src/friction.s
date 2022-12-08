ac9b956b38799f34d1fb4cd68bd1f1eb2c6d7912
Ozzyboshi <gun101@email.it>
2022-12-08 09:53:15 +0100

    friction cleanup

--- rc_ocs/src/friction.s
+++ rc_ocs/src/friction.s
@@ -8,7 +8,7 @@
 FRICTION_VECTOR: dc.l 0
 
 APPLY_FRICTION:
-    
+
     tst.w MOVER_IS_ACCELERATING_OFFSET(a0)
     bne.w end_apply_friction
 
@@ -17,10 +17,11 @@ APPLY_FRICTION:
 
     movem.l a0/d7,-(sp)
     move.l a2,a0
+
     ; friction vector = velocity vector * -1
     ; we need this to have the vector pointing to the opposite direction
-    move.w MOVER_X_VELOCITY_OFFSET(a0),d0
-    move.w MOVER_Y_VELOCITY_OFFSET(a0),d1
+    STORECARPROPERTY MOVER_X_VELOCITY_OFFSET,d0
+    STORECARPROPERTY MOVER_Y_VELOCITY_OFFSET,d1
     neg.w  d0
     neg.w  d1
 
@@ -31,28 +32,19 @@ APPLY_FRICTION:
     ;FRICTION_VECTOR now holds the opposite direction of VELOCITY VECTOR
     ; we must scale it according to friction factor
     move.w MOVER_FRICTION_COEFFICIENT_OFFSET(a2),d7
-    move.w               #%10000000,d7
-    ;DEBUG 0001
-    ;jsr SET2DMAGNITUDE
     jsr SET2DMAGNITUDE_Q4_12_TABLE_LOOKUP
-     ;   DEBUG 0002
-
 
     IFD DEBUG
     DV_DIV #20,#64,(a0),#4
     ENDC
 
     ; put velocity into a1
-    SETCARPROPERTYADDR MOVER_VELOCITY_OFFSET,a1  
+    SETCARPROPERTYADDR MOVER_VELOCITY_OFFSET,a1
     ; a1 now points to the velocity vector
 
     ; a0 is set to friction vector
     ; a1 is set to car velocity vector
     ADD2DVECTOR
-    ;IFD DEBUG
-    ;DV_DIV #160,#64,(a1),#64
-    ;ENDC
-    
 
     movem.l (sp)+,a0/d7
 end_apply_friction:
