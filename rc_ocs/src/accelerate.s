abba16b189fe2b9a31c00ab9077e973aacfaa3d4
Ozzyboshi <gun101@email.it>
2022-12-26 17:01:15 +0100

    accelerate code cleanup

--- rc_ocs/src/accelerate.s
+++ rc_ocs/src/accelerate.s
@@ -1,6 +1,6 @@
 ; Accelerate routine
 ; Change velocity according to the acceleration vector
-; acceleration vector is scaled according to engine power
+; Acceleration vector is scaled according to engine power
 ; and takes direction from forward vector
 ; Car pointer must be in a0
 
@@ -21,21 +21,20 @@ do_acceleration:
 
     ; Create a normalized vector in Q4,12 format into the acceleration vector
     SETCARPROPERTYADDR MOVER_ACCELERATION_OFFSET,a0
-	CREATE2DVECTORFROMANGLE
+    CREATE2DVECTORFROMANGLE
 
-    ;NORMALIZEVECTOR #4,(a0)
     SETCARPROPERTYADDR MOVER_ENGINE_POWER_OFFSET,a0
     SETCARPROPERTYADDR MOVER_ACCELERATION_OFFSET,a1
     DIV2DVECTOR1X2
 
     ; now the acceleration vector is scaled according to the engine power
     IFD DEBUG
-    DV                  #160,#20,(a1),#1 ; prima fila centrale
+    DV                  #160,#20,(a1),#1
     ENDC
 
     ; add accelleration to velocity
     SETCARPROPERTYADDR MOVER_ACCELERATION_OFFSET,a0  ; a0 now points to the acceleration vector
-    SETCARPROPERTYADDR MOVER_VELOCITY_OFFSET,a1             ; a1 now points to the velocity vector
+    SETCARPROPERTYADDR MOVER_VELOCITY_OFFSET,a1      ; a1 now points to the velocity vector
 
     ; a0 is set to car acceleration vector*engine_power
     ; a1 is set to car velocity vector
