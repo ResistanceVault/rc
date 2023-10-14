a2aeec6348c6f28315289f7fd2db15e4feb6a4b7
Ozzyboshi <gun101@email.it>
2023-10-14 16:18:11 +0200

    add road9 track

---
+++ rc_ocs/assets/tracks/road9/metadata/road9.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="road9" tilewidth="320" tileheight="240" tilecount="1" columns="1">
+ <image source="../images/road9_32c.png" width="320" height="240"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="trajectory" type="trajectory" x="149" y="54.5">
+    <polygon points="-2.5,3 -71,-34.25 -108.25,-28.5 -114.25,-7.75 -109.5,38.75 -74.75,62 -70.25,102.75 -76.5,124.75 -49.5,151.5 -26,141.5 -22,124.5 -20.5,104.25 -11.25,84.75 20.25,75 50,98 91.75,117 125.75,100.5 131.5,82.5 122.75,61.5 100,51.5 40.25,24.75"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
