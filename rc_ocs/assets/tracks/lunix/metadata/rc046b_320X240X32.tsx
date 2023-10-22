23e31066ac6464c94fd7f4b3a2f54da0e1615147
Ozzyboshi <gun101@email.it>
2023-10-22 13:17:42 +0200

    lunix track version 2

---
+++ rc_ocs/assets/tracks/lunix/metadata/rc046b_320X240X32.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="rc046b_320X240X32" tilewidth="320" tileheight="240" tilecount="1" columns="1">
+ <image source="../images/rc046b_320X240X32.png" width="320" height="240"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="trajectory" type="trajextory" x="124" y="34.3333">
+    <polygon points="0,0 -90.3333,1.66667 -93,32 -88.3333,57.6667 -76.3333,63.6667 -21.3333,57 3,71 6.33333,96.3333 -45,106 -88.3333,104.667 -96,119.667 -99,147.667 -84.6667,170.667 -11.6667,160 74.6667,79 153,70.3333 163.333,108 136.333,125 88.3333,124.333 71.3333,153 83.6667,173.333 117.333,174.333 125,146.667 127.667,26.3333 72.6667,2.66667"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
